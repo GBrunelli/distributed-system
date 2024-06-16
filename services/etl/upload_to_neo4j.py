d2fe465de140ae4baecc3ffe2e33044d2abb4b5b
Gustavo H Brunelli <gubrunelli@hotmail.com>
2024-06-16 16:51:02 -0300

    simplify

--- services/etl/upload_to_neo4j.py
+++ services/etl/upload_to_neo4j.py
@@ -1,24 +1,10 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import lit
 import pandas as pd
 from neo4j import GraphDatabase
 
-# Initialize Spark session
-spark = SparkSession.builder \
-    .appName("XLS to Neo4j") \
-    .getOrCreate()
-
-# Read XLS file into a DataFrame
-xls_file_path = "/path/to/your/file.xls"
-df = pd.read_excel(xls_file_path)
-
-# Convert Pandas DataFrame to Spark DataFrame
-spark_df = spark.createDataFrame(df)
-
 # Define Neo4j connection details
-neo4j_uri = "bolt://test:7687"
+neo4j_uri = "neo4j://neo4j-headless.neo4j.svc.cluster.local:7687"
 neo4j_user = "neo4j"
-neo4j_password = "password"
+neo4j_password = "bmVvNGo6bXlwYXNzd29yZA=="
 
 # Function to create Neo4j driver
 def get_neo4j_driver(uri, user, password):
@@ -28,9 +14,9 @@ def get_neo4j_driver(uri, user, password):
 def create_nodes(tx, rows):
     query = """
     UNWIND $rows AS row
-    CREATE (n:YourLabel {field1: row.field1, field2: row.field2, ...})
+    CREATE (n:YourLabel)
     """
-    tx.run(query, rows=rows.to_dict('records'))
+    tx.run(query, rows=rows)
 
 # Function to run Neo4j transactions
 def run_neo4j_transactions(rows):
@@ -38,8 +24,14 @@ def run_neo4j_transactions(rows):
     with driver.session() as session:
         session.write_transaction(create_nodes, rows)
 
+# Read XLS file into a DataFrame
+xls_file_path = "xls_conformidade_site_20240604_162827951.xls"
+df = pd.read_excel(xls_file_path)
+
+# Convert DataFrame rows to a list of dictionaries
+rows = df.to_dict('records')
+
 # Run the transaction
-rows = spark_df.collect()
 run_neo4j_transactions(rows)
 
-spark.stop()
+print("Data uploaded to Neo4j successfully!")
